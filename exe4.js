857931747b670cbb6c2a1e084fca6d04b9bef16a
Diego <diegoa.velascoc@gmail.com>
2020-10-20 15:20:26 -0700

    exercise 4.1

--- exe4.js
+++ exe4.js
@@ -1,14 +1,36 @@
+/* eslint-disable prefer-const */
+/* eslint-disable camelcase */
+
 const fs = require('fs');
 const ip2loc = require('ip2location-nodejs');
 
-function getDataFromLog () {
+class Ip2 {
+    constructor (obj) {
+        this.path = process.env.FILE;
+        this.ip2loc = obj;
+    }
+
+    init () {
+        this.ip2loc.IP2Location_init(this.path || 'IP2LOCATION-LITE-DB1.BIN');
+    }
+
+    close () {
+        this.ip2loc.IP2Location_close();
+    }
+
+    getInformationIP (ip) {
+        return this.ip2loc.IP2Location_get_all(ip);
+    }
+}
+
+function getDataFromLog (path) {
     return new Promise((resolve, reject) => {
-        fs.readFile('./auth.log.1', 'utf8', (err, data) => {
+        fs.readFile(path, 'utf8', (err, data) => {
             if (err) {
                 reject(err);
             }
             try {
-                resolve(getArre_User_Ip(data));
+                resolve(data);
             } catch (error) {
                 reject(error);
             }
@@ -16,37 +38,41 @@ function getDataFromLog () {
     });
 }
 
-function getArre_User_Ip(file) {
-    let user_ip_regex = /Invalid user (\w+) from (\b((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\.)){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\b)/mg;
-    let match = user_ip_regex.exec(file);
+function getArre_User_Ip (file, regex) {
+    let match = regex.exec(file);
     let arreUser_Ip = [];
     do {
         arreUser_Ip.push({ user: match[1], ip: match[2] });
-    } while ((match = user_ip_regex.exec(file)) !== null);
-    return frequencyCountry(arreUser_Ip);
+    } while ((match = regex.exec(file)) !== null);
+    return arreUser_Ip;
 }
 
-function frequencyCountry (arre) {
-    const reducer = (obj, useripObj) => {
-        let information = ip2loc.IP2Location_get_all(useripObj.ip);
-        let country = information.country_long;
+function frequencyCountry (arreNameIP) {
+    const ip2locObj = new Ip2(ip2loc);
+    const reducer = (obj, { ip }) => {
+        let infoIp = ip2locObj.getInformationIP(ip);
+        let country = infoIp.country_long;
         if (obj[country] === undefined) obj[country] = 1;
         else obj[country] += 1;
         return obj;
     };
-    ip2loc.IP2Location_init('IP2LOCATION-LITE-DB1.BIN');
-    const countries = arre.reduce(reducer, {});
-    ip2loc.IP2Location_close();
+    ip2locObj.init();
+    const countries = arreNameIP.reduce(reducer, {});
+    ip2locObj.close();
     return countries;
 }
 
-function createTable(data) {
+function createTable (data) {
     console.table(data);
 }
-
-async function init() {
-    let data = await getDataFromLog();
-    createTable(data);
-    return data;
+//command FILE='IP2LOCATION-LITE-DB1.BIN' LOG='auth.log.1' node exe4.js
+async function init () {
+    let regexIP = /Invalid user (\w+) from (\b((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\.)){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\b)/mg;
+    let dataFile = await getDataFromLog(process.env.LOG);
+    let arreUserIp = getArre_User_Ip(dataFile, regexIP);
+    let frequency = frequencyCountry(arreUserIp);
+    createTable(frequency);
 }
-module.exports = init;
+
+// init();
+module.exports = { getDataFromLog, getArre_User_Ip, frequencyCountry };
--- test/exe4.test.js
+++ test/exe4.test.js
@@ -1,78 +1,89 @@
-let init = require('../exe4');
+/* eslint-disable camelcase */
+/* eslint-disable prefer-const */
+/* eslint-disable quote-props */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
 
-test('get Table from .log', async () => {
-    let object = {
-        'Malaysia': 3,
-        'China': 371,
-        'France': 87,
-        'Netherlands': 67,
-        'Germany': 53,
-        'Russian Federation': 31,
-        'Mauritius': 3,
-        'Ukraine': 4,
-        'Canada': 30,
-        'South Africa': 1,
-        'Indonesia': 23,
-        'Singapore': 22,
-        'United States of America': 107,
-        'Romania': 2,
-        'Korea (Republic of)': 37,
-        'Argentina': 7,
-        'India': 36,
-        'United Kingdom of Great Britain and Northern Ireland': 21,
-        'Brazil': 72,
-        'Panama': 2,
-        'Japan': 3,
-        'Taiwan (Province of China)': 10,
-        'Viet Nam': 66,
-        'Sweden': 5,
-        'Italy': 15,
-        'Poland': 5,
-        'Saudi Arabia': 1,
-        'Colombia': 11,
-        'United Arab Emirates': 5,
-        'Hong Kong': 20,
-        'Zimbabwe': 2,
-        'Chile': 5,
-        'Iran (Islamic Republic of)': 9,
-        'Serbia': 1,
-        'Bolivia (Plurinational State of)': 4,
-        'Mexico': 11,
-        'Paraguay': 4,
-        'Hungary': 5,
-        'Moldova (Republic of)': 1,
-        'Czechia': 1,
-        'Lithuania': 1,
-        "Cote D'ivoire": 1,
-        'Azerbaijan': 1,
-        'Australia': 3,
-        'Spain': 8,
-        'Thailand': 8,
-        'Ecuador': 2,
-        'Philippines': 4,
-        'Dominican Republic': 1,
-        'Peru': 2,
-        'Bangladesh': 2,
-        'Greece': 1,
-        'Slovenia': 2,
-        'Botswana': 1,
-        'Turkey': 1,
-        'Kazakhstan': 1,
-        'Nepal': 2,
-        'Uzbekistan': 1,
-        'Puerto Rico': 1,
-        'Morocco': 1,
-        'Croatia': 6,
-        'Sri Lanka': 1,
-        'Mongolia': 1,
-        'Guatemala': 1,
-        'Bulgaria': 1,
-        'Ireland': 1,
-        'Zambia': 1,
-        'Algeria': 1,
-        'Portugal': 1,
-        'Uganda': 1
-    };
-    let data = await init();
-    expect(data).toEqual(object);
+///      this command was use to do the test --->   npm run test:four 'test.log.1' 'test2.log.1'
+
+let { getDataFromLog, getArre_User_Ip, frequencyCountry } = require('../exe4');
+
+test('testing getDataFromLoG, get message from a log', async () => {
+    const message = 'Invalid user one from 1.2.127.255\n' +
+                    'Invalid user two from 1.3.255.255\n' +
+                    'Invalid user three from 1.4.127.255\n' +
+                    'Invalid user four from 1.8.255.255\n' +
+                    'Invalid user five from 1.10.9.255\n' +
+                    'Invalid user six from 1.10.127.255\n' +
+                    'Invalid user seven from 1.15.255.255';
+    const log = process.argv[4];
+    let data = await getDataFromLog(log);
+    expect(data).toBe(message);
+});
+
+test('testing getDataFromLoG, get message from a log', async () => {
+    const message = 'Invalid user oneJapan from 1.21.255.255\n' +
+                    'Invalid user twoJapan from 1.32.231.255\n' +
+                    'Invalid user threeJapan from 1.33.129.23\n' +
+                    'Invalid user fourJapan from 1.33.213.198\n' +
+                    'Invalid user fiveJapan from 1.33.213.225\n' +
+                    'Invalid user sixJapan from 1.33.255.255\n' +
+                    'Invalid user sevenJapan from 1.67.255.255';
+    const log = process.argv[5];
+    let data = await getDataFromLog(log);
+    expect(data).toBe(message);
+});
+
+test('gettin the right number of elements in the array from element', async () => {
+    const log = process.argv[4];
+    let data = await getDataFromLog(log);
+    let regexIP = /Invalid user (\w+) from (\b((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\.)){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\b)/mg;
+    let arreData = getArre_User_Ip(data, regexIP);
+
+    const result =[{"ip": "1.2.127.255", "user": "one"},
+        {"ip": "1.3.255.255", "user": "two"},
+        {"ip": "1.4.127.255", "user": "three"},
+        {"ip": "1.8.255.255", "user": "four"},
+        {"ip": "1.10.9.255", "user": "five"},
+        {"ip": "1.10.127.255", "user": "six"},
+        {"ip": "1.15.255.255", "user": "seven" }
+    ];
+    expect(arreData).toEqual(result);
+});
+
+test('gettin the right number of elements in the array from element', async () => {
+    const log = process.argv[5];
+    let data = await getDataFromLog(log);
+    let regexIP = /Invalid user (\w+) from (\b((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\.)){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\b)/mg;
+    let arreData = getArre_User_Ip(data, regexIP);
+
+    const result =[{"ip": "1.21.255.255", "user": "oneJapan"},
+        {"ip": "1.32.231.255", "user": "twoJapan"},
+        {"ip": "1.33.129.23", "user": "threeJapan"},
+        {"ip": "1.33.213.198", "user": "fourJapan"},
+        {"ip": "1.33.213.225", "user": "fiveJapan"},
+        {"ip": "1.33.255.255", "user": "sixJapan"},
+        {"ip": "1.67.255.255", "user": "sevenJapan" }
+    ];
+    expect(arreData).toEqual(result);
+});
+
+test('testing frequency for chinase Ips', async () => {
+    const log = process.argv[4];
+    let data = await getDataFromLog(log);
+    let regexIP = /Invalid user (\w+) from (\b((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\.)){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\b)/mg;
+    let arreData = getArre_User_Ip(data, regexIP);
+    let frequency = frequencyCountry(arreData);
+    expect(frequency).toEqual({'China': 7});
+});
+
+test('testing frequency for japanese Ips', async () => {
+    const log = process.argv[5];
+    let data = await getDataFromLog(log);
+    let regexIP = /Invalid user (\w+) from (\b((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\.)){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\b)/mg;
+    let arreData = getArre_User_Ip(data, regexIP);
+    let frequency = frequencyCountry(arreData);
+    expect(frequency).toEqual({'Japan': 7});
 });
